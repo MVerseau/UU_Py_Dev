e3893b9658f4194505bdf47d09fd26b51c301a01
Марина <168333708+MVerseau@users.noreply.github.com>
2024-05-01 22:49:50 +0300

    Add files via upload

---
+++ 2.6.defs.py
@@ -0,0 +1,9 @@
+def print_params(params):
+    print(str(params) * 2)
+
+
+print_params("Hello World")
+print_params('One more time')
+print_params(True)
+print_params(123)
+print_params('This is the last time')
