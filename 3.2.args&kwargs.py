19a823be6d314cde606c250c4409f39eef97f235
Марина <168333708+MVerseau@users.noreply.github.com>
2024-05-02 23:39:23 +0300

    Add files via upload

---
+++ 3.2.args&kwargs.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [1, 'string', False]
+values_dict = {'a': 1, 'b': 'string', 'c': False}
+
+# print_params(*values_list, **values_dict) 
+
+values_list_2 = [1, 'string']
+print_params(*values_list_2, 42)
