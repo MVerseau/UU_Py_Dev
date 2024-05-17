de4d4a153d4e6cf67e6d89353967387f6688e57e
Марина <168333708+MVerseau@users.noreply.github.com>
2024-05-17 15:56:42 +0300

    Update Module6task2.py

--- Module6task2.py
+++ Module6task2.py
@@ -17,5 +17,10 @@ class Nissan(Car, Vehicle):
         return '2'
 
 
+
 nissan = Nissan()
-print(nissan.vehicle_type, nissan.price)
+print(nissan.vehicle_type, nissan.price, nissan.horse_powers(),'- атрибуты и методы экземпляра класса Nissan.')
+print(Nissan.vehicle_type, Nissan.price, Nissan.horse_powers(Nissan),'- атрибуты и методы класса Nissan.')
+print(Car.price, Car.horse_powers(Car),'- атрибуты и методы класса Nissan, атрибута "vehicle_type" у него нет.')
+print(Vehicle.vehicle_type, f'({type(Vehicle.vehicle_type)})','- атрибуты и методы класса Nissan, атрибута "price" и "horse_powers" у него нет.')
+
