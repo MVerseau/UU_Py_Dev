a80c851768cf5ac748b976c5522e9e318a8daf16
Марина <168333708+MVerseau@users.noreply.github.com>
2024-06-18 09:46:39 +0300

    Update Module10task1.py

--- Module10task1.py
+++ Module10task1.py
@@ -1,25 +1,22 @@
 from threading import Thread
 from time import sleep
 
-def interv(start,end):
+
+def interv(start, end):
     if isinstance(start, str):
-        for i in range(ord(start),ord(end)+1):
+        for i in range(ord(start), ord(end) + 1):
             print(chr(i))
-            
+            sleep(1)
     else:
-        for i in range(start, end+1):
+        for i in range(start, end + 1):
             print(i)
-            
-        
-        
-        
-t1=Thread(target=interv, kwargs=dict(start=1, end=10))
-# t2=Thread(target=interv, kwargs=dict(start='a', end='j'))
+            sleep(1)
+
+
+t1 = Thread(target=interv, kwargs=dict(start=1, end=10))
+t2 = Thread(target=interv, kwargs=dict(start='a', end='j'))
 
 t1.start()
-# t2.start()
-# interv(1,10)
-interv('a','j')
-# sleep(1)
+t2.start()
 t1.join()
-# t2.join()
+t2.join()
