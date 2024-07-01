11ca9ec0ecbb0d036a0ee0924b5cf7387a1b3d16
Марина <168333708+MVerseau@users.noreply.github.com>
2024-07-01 18:09:55 +0300

    Update Module10task4.py

--- Module10task4.py
+++ Module10task4.py
@@ -19,34 +19,35 @@ class Cafe:
         self.tables_lock = threading.Lock()
 
     def customer_arrival(self):  # функция-producer
-        for i in range(1,21):
+        for i in range(1, 21):
+            time.sleep(1)
             c = Customer(func=self.serve_customer)
             c.name = f'{i}'
             print(f'Посетитель номер {c.name} прибыл.')
-            self.queue.put(c)
-            time.sleep(1)
+            c.start()
+            # self.queue.put(c)
+
 
 
     def serve_customer(self, customer):  # функция-customer #FIXME: Зациклился - приходит и есть только посетитель 1
 
-        if all([table.is_busy for table in tables]): #False
+        if all([table.is_busy for table in tables]):  # False
             print(f'Посетитель номер {customer.name} ожидает свободный стол.')
             event.clear()
             event.wait()
         for table in self.tables:
+
             if not table.is_busy:
                 table.is_busy = True
-
                 print(f'Посетитель номер {customer.name} сел за стол {table.number}.')
                 time.sleep(5)
-
-                print(f'Посетитель номер {customer.name} покушал и ушёл.')
                 table.is_busy = False
                 # self.queue.task_done()
                 event.set()
+                print(f'Посетитель номер {customer.name} покушал и ушёл.')
 
                 break
-        print(f'Количество потоков {threading.active_count()}')
+        # print(f'Количество потоков {threading.active_count()}')
         # self.queue.join()
 
 
@@ -54,11 +55,12 @@ class Customer(threading.Thread):
     def __init__(self, func):
         super().__init__()
         self.func = func
+
     def run(self):
         self.func(self)
 
 
-event=threading.Event()
+event = threading.Event()
 event.set()
 
 # Создаем столики в кафе
