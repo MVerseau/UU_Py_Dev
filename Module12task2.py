73a5f740ec5112af1a23a83c4c1bff5b7bffd36c
Марина <168333708+MVerseau@users.noreply.github.com>
2024-07-19 23:58:17 +0300

    Add files via upload

---
+++ Module12task2.py
@@ -0,0 +1,44 @@
+import runner_and_tournament
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    @classmethod
+    def tearDownClass(cls):
+        for item in cls.all_results:
+            print({k:v.name for k,v in item.items()})
+        # print(cls.all_results)
+
+    def setUp(self):
+        self.rn1 = runner_and_tournament.Runner('Усэйн', speed=10)
+        self.rn2 = runner_and_tournament.Runner('Андрей', speed=9)
+        self.rn3 = runner_and_tournament.Runner('Ник', speed=3)
+
+
+    def test_run1(self):
+        part_tuple=(self.rn3, self.rn1)
+        results = runner_and_tournament.Tournament(90,*part_tuple ).start()
+        TournamentTest.all_results.append(results)
+        # print({k: v.name for k, v in results.items()})
+        self.assertTrue(sorted(results.items(), key=lambda i: i[0])[-1][1] == sorted(part_tuple, key=lambda i: i.speed, reverse=True)[-1].name)
+
+    def test_run2(self):
+        part_tuple = (self.rn3, self.rn2)
+        results = runner_and_tournament.Tournament(90, *part_tuple).start()
+        TournamentTest.all_results.append(results)
+        # print({k: v.name for k, v in results.items()})
+        self.assertTrue(sorted(results.items(), key=lambda i: i[0])[-1][1] == sorted(part_tuple, key=lambda i: i.speed, reverse=True)[-1].name)
+
+    def test_run3(self):
+        part_tuple = (self.rn3, self.rn2, self.rn1)
+        results = runner_and_tournament.Tournament(90, *part_tuple).start()
+        TournamentTest.all_results.append(results)
+        # print({k: v.name for k, v in results.items()})
+        self.assertTrue(sorted(results.items(), key=lambda i: i[0])[-1][1] == sorted(part_tuple, key=lambda i: i.speed, reverse=True)[-1].name)
+
+if __name__ == '__main__':
+    unittest.main()
