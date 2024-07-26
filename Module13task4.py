548f9c296cefa6e6d5d670b5aa1759aa39c4cde0
Марина <168333708+MVerseau@users.noreply.github.com>
2024-07-26 20:30:17 +0300

    Add files via upload

---
+++ Module13task4.py
@@ -0,0 +1,31 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from aiogram.filters.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+dp = Dispatcher()
+api = 
+bot = Bot(token=api)
+
+
+class UserState(StatesGroup):
+    address = State()
+
+
+@dp.message(Command('start'))
+async def buy(message,state):
+    await message.answer('Отправьте на свой адрес, пожалуйста.')
+    await state.set_state(UserState.address)
+
+
+@dp.message(UserState.address)
+async def fsm_handler(message, state):
+    await state.update_data(first=message.text)
+    date = await state.get_data()
+    await message.answer(f'Доставка будет отправлена на {date['first']}')
+    await state.clear()
+
+
+if __name__ == "__main__":
+    asyncio.run(dp.start_polling(bot))
