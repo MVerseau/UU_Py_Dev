6b4c7e6f8fd83c27e25826e1c61417eb934655e1
Марина <168333708+MVerseau@users.noreply.github.com>
2024-09-22 23:49:49 +0300

    Update Module16task3.py

--- Module16task3.py
+++ Module16task3.py
@@ -11,26 +11,33 @@ async def get_users() -> dict:
     return users
 
 
-@app.post('/users/{username}/{age}')
+@app.post('/user/{username}/{age}')
 async def new_user(username: Annotated[
     str, Path(min_length=1, max_length=20, description='Enter your username', example='Myname')],
-                   age: Annotated[int, Path(ge=18, min_length=2, description='How old are you?', example=20)]) -> str:
+                   age: Annotated[int, Path(ge=18, description='How old are you?', example=20)]) -> str:
     max_index = str(int(max(users, key=int)) + 1)
     users[max_index] = f'Имя: {username}, возраст: {age}'
     return f"User {max_index} is registered"
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def change_user(user_id: Annotated[int, Path(ge=0, le=int(max(users, key=int)))],
+async def change_user(user_id: str,
                       username: Annotated[
                           str, Path(min_length=1, max_length=20, description='Enter your username', example='Myname')],
                       age: Annotated[
-                          int, Path(ge=18, min_length=2, description='How old are you?', example=20)]) -> str:
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f"User {user_id} has been updated"
+                          int, Path(ge=18, description='How old are you?', example=20)]) -> str:
+    try:
+        users[user_id]
+        users[user_id] = f'Имя: {username}, возраст: {age}'
+        return f"User {user_id} has been updated"
+    except:
+        return f'User with ID {user_id} has not been found'
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: Annotated[str, Path(ge=0, le=int(max(users, key=int)))]) -> str:
-    users.pop(user_id)
-    return f"User {user_id} has been deleted"
+async def delete_user(user_id: str) -> str:
+    try:
+        users.pop(user_id)
+        return f"User {user_id} has been deleted"
+    except:
+        return f'User with ID {user_id} has not been found'
