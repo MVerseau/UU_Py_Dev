28a6da87817b7b91120e6476b4a56bfa0652ce3d
Марина <168333708+MVerseau@users.noreply.github.com>
2024-10-07 22:04:09 +0300

    Add files via upload

---
+++ MyBlog/Blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post
+
+
+class PostsPerPage(forms.Form):
+    choices = (
+        ('3', 'по 3 поста'),
+        ('5', "по 5 постов"),
+        (str(len(Post.objects.all())), "Все посты"),
+
+    )
+    number = forms.ChoiceField(choices=choices, widget=forms.Select(), required=False, label='На странице')
